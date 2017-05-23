bd5693aef8ec402fe47199d61c1ab9442fffc445
Frédéric LETESSIER <fletessier@sii.fr>
2017-05-23 13:02:33 +0200

    #4 ajout d'un fichier 'hello world'

---
+++ setup.py
@@ -0,0 +1,21 @@
+﻿
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='homebot',
+    version='0.0.1',
+    description='Chatbot app',
+    long_description=readme,
+    author='fletessier',
+    author_email='',
+    url='https://github.com/fletessier/homebot',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
